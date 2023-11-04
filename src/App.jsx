7c3a356b9bd060b50dd65fbd05cbac22b8971ec9
Andres <LEANMUAG@outlook.com>
2023-11-04 02:30:47 -0500

    feat(layout):brief layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar"; // Importa una sola vez aquí
-import Home from "./pages/home";
-import Contact from "./pages/Contact";
-import About from "./pages/About";
-import Services from "./pages/Services"
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar';
+import Footer from './components/Footer';
+import Home from './pages/home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Services from './pages/Services'; // Asumiendo que tienes una página de servicios
 
 function App() {
   return (
@@ -15,7 +17,9 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
+        {/* Agrega más rutas según sea necesario */}
       </Routes>
+      <Footer />
     </Router>
   );
 }
