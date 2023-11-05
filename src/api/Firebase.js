de85e9a24a27d390e4b7fd8945857f59e10adf98
Andres <LEANMUAG@outlook.com>
2023-11-05 04:26:36 -0500

    firebase

--- src/api/Firebase.js
+++ src/api/Firebase.js
@@ -1,14 +1,25 @@
 import firebase from 'firebase/app';
-import 'firebase/database'; // Si estás utilizando la base de datos en tiempo real
+import 'firebase/firestore'; // si vas a usar Firestore
+import 'firebase/auth'; // si vas a usar la autenticación
 
-// Tu configuración de Firebase
 const firebaseConfig = {
-  // ... tu configuración ...
+  apiKey: "tu_api_key",
+  authDomain: "tu_auth_domain",
+  projectId: "tu_project_id",
+  storageBucket: "tu_storage_bucket",
+  messagingSenderId: "tu_messaging_sender_id",
+  appId: "tu_app_id",
+  measurementId: "tu_measurement_id"
 };
 
-// Inicializar Firebase
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
+// Inicializa Firebase
+firebase.initializeApp(firebaseConfig);
 
-// Usar Firebase aquí
+// Si vas a utilizar Firestore
+const db = firebase.firestore();
+
+// Si vas a utilizar la autenticación
+const auth = firebase.auth();
+
+// Exporta las constantes para usarlas en otros archivos
+export { db, auth };
